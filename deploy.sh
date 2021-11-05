3f097d5f7fe88a64d1b3fb9035ecdd188609d614
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2021-11-05 12:07:05 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ source venv/bin/activate
 
 pip3 install -r requirements.txt
 
-export db_uri=$db_uri 
-export secretkey=$secretkey
+export db_uri
+export secretkey
 
 python3 create.py 
 python3 app.py

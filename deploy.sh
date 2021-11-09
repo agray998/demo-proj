af05edb31d59e558ad5fceba6960a961b27ff3c3
agray998 <a.gray.4797@gmail.com>
2021-11-09 13:11:00 +0000

    sorting out systemd service

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 sudo apt install python3 python3-pip python3-venv gunicorn -y
 
-python3 -m venv venv
-source venv/bin/activate
+#python3 -m venv venv
+#source venv/bin/activate
 
 pip3 install -r requirements.txt
 

9ad23aa75d88dae489a1d83e51f39ab64326bd9d
agray998 <a.gray.4797@gmail.com>
2021-11-09 09:46:22 +0000

    removed -D

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,5 @@ fi
 echo 'TESTING:'
 python3 -m pytest --cov=application --cov-report html
 
-gunicorn --workers=4 --bind=0.0.0.0:5000 app:app
+python3 app.py
+#gunicorn --workers=4 --bind=0.0.0.0:5000 app:app
